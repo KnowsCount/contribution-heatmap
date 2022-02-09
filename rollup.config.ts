8ca8598ca8d8e3e8e763481c762bc18ef23d6347
KnowsCount <knowscount@gmail.com>
2022-02-10 02:37:52 +0800

    chore: code styles

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,37 +1,37 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import postcss from 'rollup-plugin-postcss'
+import dts from 'rollup-plugin-dts'
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json')
 
 export default [
-  	{
-		input: "src/index.ts",
+	{
+		input: 'src/index.ts',
 		output: [
 			{
 				file: packageJson.main,
-				format: "cjs",
+				format: 'cjs',
 				sourcemap: true,
 			},
 			{
 				file: packageJson.module,
-				format: "esm",
+				format: 'esm',
 				sourcemap: true,
 			},
 		],
 		plugins: [
 			resolve(),
 			commonjs(),
-			typescript({ tsconfig: "./tsconfig.json" }),
+			typescript({ tsconfig: './tsconfig.json' }),
 			postcss(),
 		],
 	},
 	{
-		input: "dist/esm/types/index.d.ts",
-		output: [{ file: "dist/index.d.ts", format: "esm" }],
+		input: 'dist/esm/types/index.d.ts',
+		output: [{ file: 'dist/index.d.ts', format: 'esm' }],
 		plugins: [dts()],
 		external: [/\.(css|less|scss)$/],
 	},
-];
+]
