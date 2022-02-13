234bcf8f96c47ba27a3f75db6f7dca6e68b36296
KnowsCount <knowscount@gmail.com>
2022-02-13 22:19:26 +0800

    feat: create utils.ts & misc types

--- src/heatmap/Heatmap.tsx
+++ src/heatmap/Heatmap.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import { months, weekDays, daysInYear } from '../constants'
+import { generateRandomData } from '../utils'
 import './Heatmap.css'
+import type { Day } from '../types'
+
+type Data = Array<Day>
+
+export interface IProps {
+	/**
+	 * sample data
+	 * {
+	 *     date: '2020-01-01',
+	 *     count: 2
+	 *     level: 'low'
+	 * }
+	 */
+	data: Data
+	children?: React.ReactNode
+	loading?: boolean
+	transformData?: (data: Data) => Data
+}
+
+// async function getData(url) {}
 
 const Heatmap = () => {
 	return (
@@ -19,10 +40,10 @@ const Heatmap = () => {
 				<ul className="squares">
 					{[...Array(daysInYear)].map((_, i) => (
 						<li
-							data-level={Math.floor(Math.random() * 4)}
+							data-level={generateRandomData()}
 							key={i}
 							data-tooltip={
-								Math.floor(Math.random() * 4) +
+								generateRandomData() +
 								' contributions on this day'
 							}
 						></li>
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function generateRandomData(): number {
+    return Math.floor(Math.random() * 4)
+}
