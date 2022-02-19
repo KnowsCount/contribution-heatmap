38b346010116c61b9862846711301341f55bf1e1
KnowsCount <knowscount@gmail.com>
2022-02-19 09:40:55 +0800

    chore: add example

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Heatmap } from "contribution-heatmap";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Heatmap />
+    </div>
+  );
+}
+
+export default App;
