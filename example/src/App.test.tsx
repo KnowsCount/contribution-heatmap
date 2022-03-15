31c4c35481995ffb3df56387efa6ac8e969bf5f5
KnowsCount <knowscount@gmail.com>
2022-03-15 21:39:43 +0800

    chore: delete example

--- example/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders dates', () => {
-	render(<App />)
-	const linkElement = screen.getByText(/Jan/i)
-	expect(linkElement).toBeInTheDocument()
-})
