5ec1074d1c26f6ecc2f0abb96c4bb4bbe0bf71e1
KnowsCount <knowscount@gmail.com>
2022-03-28 01:25:05 +0800

    feat: random colour preview

--- src/heatmap/Heatmap.stories.tsx
+++ src/heatmap/Heatmap.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { DAYS_IN_YEAR } from '../constants'
 import { Heatmap } from '../index'
-import { getRandomCount } from '../utils'
+import { getRandomCount, getRandomInt } from '../utils'
+const randomColours = require('nice-color-palettes')
 
 export default {
 	title: 'Example/Heatmap',
@@ -31,9 +32,21 @@ Default.args = {
 	},
 }
 
-export const OtherColour = () => (
-	<Heatmap
-		colour={['#ebedf0', '#e2dbe9', '#bcaecc', '#5a4565', '#3e204f']}
-		count={getRandomCount(DAYS_IN_YEAR)}
-	/>
-)
+export const PurpleTheme = Template.bind({})
+PurpleTheme.args = {
+	args: {
+		squareNumber: DAYS_IN_YEAR,
+		count: getRandomCount(DAYS_IN_YEAR),
+		colours: ['#ebedf0', '#e2dbe9', '#bcaecc', '#5a4565', '#3e204f'],
+	},
+}
+
+export const RandomColour = () => {
+	console.log(randomColours.length)
+	return (
+		<Heatmap
+			colour={randomColours[getRandomInt(0, 99)]}
+			count={getRandomCount(DAYS_IN_YEAR)}
+		/>
+	)
+}
